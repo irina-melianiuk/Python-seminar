e1a46dd1991e72d86d6e543bd8bd5ac5deca1052
Irina Melianiuk <ira87m@mail.ru>
2023-06-26 17:07:43 +0300

    задачи

--- dz_8_seminar.py
+++ dz_8_seminar.py
@@ -4,3 +4,28 @@
 Также пользователь может добавлять новых людей в справочник в режиме экспорт.
 
 """
+def search():
+    with open('phone_book.txt', 'r', encoding='utf-8') as file:
+        book = file.read().split('\n')
+        temp = input('Введите фамилию: ')
+        for i in book:
+            if temp in i:
+                print(i)
+
+def new_number():
+    surname = input('Введите фамилию: ')
+    name = input('Введите имя и отчество: ')
+    phone = input('Введите номер телефона: ')
+    with open('phone_book.txt', 'a', encoding='utf-8') as file:
+        file.writelines(f'\n{surname}\n{name}\n{phone}')
+
+
+mode = int(input('Введите режим: 1 - Поиск по справочнику, 2 - Добавление новой записи '))
+
+if mode == 1:
+    print(search())
+elif mode == 2:
+    print(new_number())
+else:
+    print('Некорректный режим')
+
